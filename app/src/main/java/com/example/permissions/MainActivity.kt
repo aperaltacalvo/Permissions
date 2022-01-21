a03823deb731b5fd10109a362e79c1fa7d4dbb67
Álvaro Peralta <alvaroperalta@MacBook-Air-de-Alvaro.local>
2022-01-21 13:22:12 +0100

    First commit with permissions example

---
+++ app/src/main/java/com/example/permissions/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.permissions
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val RECORD_REQUEST_CODE = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupPermissions()
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+
+        if(permission != PackageManager.PERMISSION_GRANTED){
+            Log.i("permi_app","Permiso denegado para grabar audio")
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.RECORD_AUDIO)){
+
+                val builder = AlertDialog.Builder(this)
+                builder.setMessage("El permiso de grabación de audio es requerido")
+                    .setTitle("permiso requerido")
+                builder.setPositiveButton("ok"){dialog,id ->
+                    makeRequest()
+                }
+                val dialog = builder.create()
+                dialog.show()
+
+            } else{
+                makeRequest()
+            }
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), RECORD_REQUEST_CODE)
+    }
+}
